08bb2c305d01ba8758aad2fb174e7a1311fa854b
Alex <andres.al.campos@gmail.com>
2018-06-13 16:44:46 -0700

    Update prog2.c
    did most of the sub-menu logic for the writeToFile() function, still have to do the processes that actually do all the work

--- prog2.c
+++ prog2.c
@@ -1,11 +1,8 @@
 
 #include "a2.h"
 
-mode_t getumask(void); 
-
-int isValidCommand(int command){
-	//if the ascii value of the character is between A-H or between a-h 
-	if ( (command >= 0 && command <= 8 ) ){
+int isValidCommand(int command, int n){
+	if ( (command >= 0 && command < n ) ){
 		return 1; //true
 	}
 	return 0; //false
@@ -14,16 +11,17 @@ int isValidCommand(int command){
 void createDirectory(){
 	char dirName[256];
 	char currDir[256];
-	//char * fullDir[256];
 	printf("enter the name of the directory file:");
 	//validate that the input is correct (doesnt contain '/' or is an empty string)
+	
 	fgets(dirName, sizeof(dirName), stdin);
+	printf("you entered: %s ",dirName);
 	//get curret directory
 	getcwd(currDir, sizeof(currDir)); 	
    	//concatonate the 2 strings
 	strcat(currDir, dirName);
 	//pass that string to mkdir("concatonated str")
-	mkdir(dirName, umask(0));
+	mkdir(dirName, umask(000));
 	printf("created directory: %s ",dirName);
 
 }
@@ -36,8 +34,58 @@ void readFromFile(){
 	printf("enter the name:");
 }
 
+void getCommand(){}
+
 void writeToFile(/*char * fileName, char * mode*/){
-	printf("enter the name:");
+	printf("enter the mode in which to open the file");
+	printf("\n1) Append to an exisiting file\n");
+	printf("2) Overwrite an existing file\n");
+	printf("3) Insert into an exisitng file\n");
+	printf("0) exit\n");
+	int command;
+	//get input from user and continue only if its valid
+	scanf("%d",&command);
+	while (!isValidCommand(command,4)){
+		printf("please enter a valid number:");
+		scanf("%d",&command); 
+	}
+	//process the users command
+	int flag = 1;
+	while (flag == 1){
+		printMainMenu();
+		// get input from the user and contine only if its valid
+		scanf("%d",&command);
+		while (!isValidCommand(command,9)){
+			printf("please enter a valid number:");
+			scanf("%d",&command); 
+		}
+		switch(command)	{
+		case 0:
+			flag = 0;
+			break;
+		case 1:
+			appendMode();
+			break;
+		case 2: 
+			overwriteMode();
+			break;
+		case 3: 
+			insertMode();
+			break;	
+		}
+	}
+}
+
+void appendMode(){
+	printf("appendMode\n");
+}
+
+void overwriteMode(){
+	printf("overwriteMode\n");
+}
+
+void insertMode(){
+	printf("insertMode\n");
 }
 
 void printFile(){
@@ -53,7 +101,7 @@ void printFileStatus(){
 }
 
 void printMainMenu(){
-	printf("\n1) Create a new directorry file\n");
+	printf("\n1) Create a new directory file\n");
 	printf("2) Create a new regular file\n");
 	printf("3) Create child process to write sorted output\n");
 	printf("4) Create child process to shadow write/read regular files\n");
@@ -73,8 +121,8 @@ int main(){
 	while (flag == 1){
 		printMainMenu();
 		// get input from the user and contine only if its valid
-		scanf("%d",&command); ///gets(command,64,stdin);
-		while (!isValidCommand(command)){
+		scanf("%d",&command);
+		while (!isValidCommand(command,8)){
 			printf("please enter a valid number:");
 			scanf("%d",&command); 
 		}
