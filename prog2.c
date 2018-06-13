b67fb4975ba1e8d71e0c90c08589018ef96ee7e5
Alex <andres.al.campos@gmail.com>
2018-06-13 03:54:43 +0000

    added some more stuff, almost finished createDirectory function

--- prog2.c
+++ prog2.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-//hi
+#include <sys/stat.h> // mkdir
+#include <sys/types.h>
+#include <unistd.h> 
+mode_t getumask(void); 
 
-int isValidCommand(char command){
+int isValidCommand(int command){
 	//if the ascii value of the character is between A-H or between a-h 
-	if ( (command >= 65 && command <= 72) || (command >= 97 && command <= 104) ){
+	if ( (command >= 0 && command <= 8 ) ){
 		return 1; //true
 	}
 	return 0; //false
 }
 
-void createDirectory(/*char * dirName*/){
-	printf("enter the name:");
-	
+void createDirectory(){
+	char * dirName[256];
+	char * currDir[256];
+	//char * fullDir[256];
+	printf("enter the name of the directory file:");
 	//validate that the input is correct (doesnt contain '/' or is an empty string)
+	fgets(dirName, sizeof(dirName), stdin);
+	//get curret dir 
+	getcwd(currDir, sizeof(currDir)); 	
+   	//concatonate the 2 strings
+	strcat(currDir, dirName);
+	//pass that string to mkdir("concatonated str")
+	mkdir(dirName, getumask());
+	printf("created directory %s succesfully",dirName);
+
 
 }
 
@@ -34,68 +48,65 @@ void printFile(){
 }
 
 void printDir(){
-	
+
 }
 
 int main(){
 	printf("Welcome to the super multiprocessing file editron 3000\n");
-	printf("A) Create a new directorry file\n");
-	printf("B) Create a new regular file\n");
-	printf("C) Create child process to write sorted output\n");
-	printf("D) Create child process to shadow write/read regular files\n");
-	printf("E) Read from a file (print out to stdout)\n");
-	printf("F) Write to a file in either insert, append, or overwrite mode\n");
-	printf("G) Print file status (print out to stdout)\n");
-	printf("H) Print directory listing (contents of dir file) (print out to stdout)\n");
+	printf("1) Create a new directorry file\n");
+
+	printf("2) Create a new regular file\n");
+	printf("3) Create child process to write sorted output\n");
+	printf("4) Create child process to shadow write/read regular files\n");
+	printf("5) Read from a file (print out to stdout)\n");
+	printf("6) Write to a file in either insert, append, or overwrite mode\n");
+	printf("7) Print file status (print out to stdout)\n");
+	printf("8) Print directory listing (contents of dir file) (print out to stdout)\n");
+	printf("0) Exit\n");
 	printf("Please select an option: ");
-	char command;
+	int command;
 
 	// get input from the user and contine only if its valid
-	command = getchar(); ///gets(command,64,stdin);
+	command = scanf("%d",command); ///gets(command,64,stdin);
 	while (!isValidCommand(command)){
-		printf("please enter a character from A to H:");
-		command = getchar();
+		printf("please enter a character input ...:");
+		command = scanf("%d",command); 
 	}
 
 	// process the user command
-	switch(command)
-	{
-		case 'a':
-		case 'A': 
+	while (1){
+
+		switch(command)	{
+		case 1:
 			createDirectory();
 			break;
-		case 'b':
-		case 'B': 
+		case 2: 
 			createFile();
 			break;
-		case 'c':
-		case 'C': 
+		case 3: 
 
 			break;	
-		case 'd':
-		case 'D': 
+		case 4: 
 
 			break;
-		case 'e':
-		case 'E': 
+		case 5: 
+ 
 			readFromFile();
 			break;
-		case 'f':
-		case 'F': 
-			writeToFile();
+		case 6: 
+			writeToFile(); //
 			break;
-		case 'g':
-		case 'G': 
+		case 7: 
 			printFileStatus();
 			break;
-		case 'h':
-		case 'H': 
+		case 8:
 			printDir();
 			break;
 		default:
 			printf("invalid command");
+		}
 	}
-
 
 	return 0;
 }
+
