3c4f330700e845ea78ebb86557c354413f70dc4e
Alex <andres.al.campos@gmail.com>
2018-06-19 17:38:45 -0700

    did a bunch of stuff

--- prog2.c
+++ prog2.c
@@ -1,9 +1,9 @@
 #include "a2.h"
 
-int c;
+int c; //used for flushing stdin to get a new command
 
 int isValidCommand(int command, int options){
-	if ( (command >= 1 && command <= options ) ){
+	if ( (command >= 1 && command <= options) ){
 		return 1; //true
 	}
 	return 0; //false
@@ -12,44 +12,47 @@ int isValidCommand(int command, int options){
 void createDirectory(){
 	char name[256];
 	char currDir[256];
-	printf("enter the name of the directory file: ");
-	while ((c = getchar()) != '\n' && c != EOF); 
-	fgets(name, sizeof(name), stdin);
+	printf("Enter the name of the directory file: ");
+	while ((c = getchar()) != '\n' && c != EOF); //flush stdin
+	scanf("%[^\n]s",name);
 	//get curret directory
-	getcwd(currDir, sizeof(currDir)); 	
+	getcwd(currDir, sizeof(currDir));
 	//add a slash then the name of the new directory
    	strcat(currDir, "/");
 	strcat(currDir, name);
 	//pass that string to mkdir()
-	mkdir(currDir, umask(0000));
-	printf("created directory: %s \n", currDir);
+	if(mkdir(currDir, 0777) == -1){
+		printf("Error creating directory you want to create\n");
+		return;
+	}
+	printf("created directory: %s", currDir);
 }
 
 void createFile(){
 	char name[256];
 	char currDir[256];
-	printf("enter the name of the file: ");
+	printf("Enter the name of the file: ");
 	while ((c = getchar()) != '\n' && c != EOF); 
-	fgets(name, sizeof(name), stdin);
+	scanf("%[^\n]s",name);
 	//store the curret directory in currDir
 	getcwd(currDir, sizeof(currDir)); 	
 	//add a slash then the name of the new directory
    	strcat(currDir, "/");
 	strcat(currDir, name);
-	//pass that string to mkdir()
-	creat(currDir, umask(0000));
-	printf("created file: %s \n", currDir);
-}
-
-void readFromFile(){
-	printf("enter the name:");
+	//pass that string to creat()
+	if (creat(currDir,S_IRWXU|S_IRWXG|S_IRWXO) == -1){
+		printf("Error creating file\n");
+		return;
+	}
+	printf("created file: %s\n", currDir);
 }
 
 void getValidCommand(int * command, int options){
+	//read in command and if its not valid prompt again
 	while (scanf("%d",command) && !isValidCommand(*command, options)){
-		// get rid of newline/EOF characters in stdin
+		//flush stdin to get new command
 		while ((c = getchar()) != '\n' && c != EOF); 
-		printf("please enter a valid number:"); 
+		printf("please enter a valid number: "); 
 	}
 }
 
@@ -60,7 +63,7 @@ void writeToFile(/*char * fileName, char * mode*/){
 		printMenu6();
 		// get input from the user and contine only if its valid
 		getValidCommand(&command,4); 
-		//process the users command
+		// process the users command
 		switch(command)	{
 		case 1:
 			appendMode();
@@ -74,40 +77,127 @@ void writeToFile(/*char * fileName, char * mode*/){
 		case 4:
 			flag = 0;
 			break;
+		default:
+			//flush stdin to get new command
+			while ((c = getchar()) != '\n' && c != EOF); 
+			printf("Invalid command\n");
+			break;
 		}
-		while ((c = getchar()) != '\n' && c != EOF); 
 	}
 }
 
+void readFromFile(){
+	char name[256];
+	char fileLine[256];
+	printf("Enter the name of the file you want to read from: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	scanf("%[^\n]s",name);
+	FILE * fd = fopen(name,"r"); 
+	if (fd == NULL){
+	    printf("Error opening file!\n");
+	    exit(1);
+	}
+	while(fgets(fileLine, sizeof(fileLine), fd) != NULL){ 
+		printf("%s",fileLine); 
+	}﻿
+	fclose(fd);
+}
+
 void appendMode(){
-	printf("appendMode\n");
+	char name[256];
+	char input[256];
+	printf("Enter the name of the file you want to append to: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	scanf("%[^\n]s",name);
+	printf("Enter what you want to write: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	fgets(input, sizeof(input), stdin);
+	FILE * fd = fopen(name,"a"); 
+	if (fd == NULL){
+	    printf("Error opening file!\n");
+	    exit(1);
+	}
+	fprintf(fd, "%s", input);
+	fclose(fd);
 }
 
 void overwriteMode(){
-	printf("overwriteMode\n");
+	char name[256];
+	char input[256];
+	printf("Enter the name of the file you want to overwrite: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	scanf("%[^\n]s",name);
+	printf("Enter what you want to write: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	fgets(input, sizeof(input), stdin);
+	FILE * fd = fopen(name,"w"); 
+	if (fd == NULL){
+	    printf("Error opening file!\n");
+	    exit(1);
+	}
+	fprintf(fd, "%s", input);
+	fclose(fd);
 }
 
 void insertMode(){
-	printf("insertMode\n");
-}
-
-void printFile(){
-	printf("enter the name:"); 
+	char name[256];
+	char input[256];
+	int offset;
+	printf("Enter the name of the file you want to insert to: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	scanf("%[^\n]s", name);
+	printf("Enter the position where you want to insert: ");
+	scanf("%d", &offset);
+	printf("Enter what you want to write: ");
+	while ((c = getchar()) != '\n' && c != EOF); // flush stdin
+	scanf("%[^\n]s",input);
+	FILE * fd = fopen(name,"wb"); 
+	if (fd == NULL){
+	    printf("Error opening file!\n");
+	    exit(1);
+	}
+	fseek(fd, offset, SEEK_SET);
+	fprintf(fd, "%s", input);
+	fclose(fd);
 }
 
 void printDir(){
-
+	DIR * d;
+	struct dirent *dir;
+	opendir();
+	readdir();
 }
 
 void printFileStatus(){
-
+	struct stat info;
+	char name[256];
+	char currDir[256];
+	printf("Enter the name of the file: ");
+	while ((c = getchar()) != '\n' && c != EOF); 
+	scanf("%[^\n]s",name);
+	//store the curret directory in currDir
+	getcwd(currDir, sizeof(currDir)); 	
+	//add a slash then the name of the new directory
+   	strcat(currDir, "/");
+	strcat(currDir, name);
+	if (stat(currDir, &info) == -1){
+		printf("Error opening file!\n");
+	    exit(1);
+	}
+	printf("  inode: %d\n", (int)info.st_ino);
+	printf(" dev id: %d\n", (int)info.st_dev);
+	printf("   mode: %08x\n", (int)info.st_mode);
+	printf("  links: %d\n", (int)info.st_nlink);
+	printf("    uid: %d\n", (int)info.st_uid);
+	printf("    gid: %d\n", (int)info.st_gid);
+	printf("   size: %d bytes\n", (int)info.st_size);
 }
 
 void printMainMenu(){
 	printf("\n1) Create a new directory file\n");
 	printf("2) Create a new regular file\n");
-	printf("3) Create child process to write sorted output\n");
-	printf("4) Create child process to shadow write/read regular files\n");
+	//printf("3) Create child process to write sorted output\n");
+	//printf("4) Create child process to shadow write/read regular files\n");
 	printf("5) Read from a file (print out to stdout)\n");
 	printf("6) Write to a file in either insert, append, or overwrite mode\n");
 	printf("7) Print file status (print out to stdout)\n");
@@ -118,9 +208,9 @@ void printMainMenu(){
 
 //print the menu for option 6 in the main menu
 void printMenu6(){
-	printf("\n1) Append to an exisiting file\n");
-	printf("2) Overwrite an existing file\n");
-	printf("3) Insert into an exisitng file\n");
+	printf("\n1) Append to a file\n");
+	printf("2) Overwrite a file\n");
+	printf("3) Insert into a file\n");
 	printf("4) exit\n");
 	printf("Please select an option: ");
 }
@@ -128,12 +218,13 @@ void printMenu6(){
 int main(){
 	printf("Welcome to the super multiprocessing file editron 3000\n");
 	int command;
-	// process the user command
 	int flag = 1;
+	// display the menu until the user exits
 	while (flag == 1){
 		printMainMenu();
 		// get input from the user and contine only if its valid
 		getValidCommand(&command,9);
+		// process the user command
 		switch(command)	{
 		case 1:
 			createDirectory();
@@ -142,30 +233,24 @@ int main(){
 			createFile();
 			break;
 		case 3: 
-
-			break;	
-		case 4: 
-
-			break;
-		case 5: 
- 
 			readFromFile();
 			break;
-		case 6: 
+		case 4: 
 			writeToFile(); //
 			break;
-		case 7: 
+		case 5: 
 			printFileStatus();
 			break;
-		case 8:
+		case 6:
 			printDir();
 			break;
-		case 9:
-			flag = 0;
+		case 7:
+			flag = 0; //exit
 			break;
 		default:
+			//flush the buffer 
 			while ((c = getchar()) != '\n' && c != EOF);
-			printf("invalid command");
+			printf("Invalid command\n");
 		} 
 	}
 	return 0;
